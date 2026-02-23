a9ad6d9ddbf0b1207d73c9dfddd9ed111723f146
southshoreai <52313863-southshoreai@users.noreply.replit.com>
2026-02-23 10:01:26 +0000

    Restored to '01252fa771957fc62cf8cf74458e6061b5f89086'
    Replit-Restored-To: 01252fa771957fc62cf8cf74458e6061b5f89086

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -19,7 +19,7 @@ function Router() {
       <Route path="/services" component={ServicesPage} />
       <Route path="/packages" component={PackagesPage} />
       <Route path="/about" component={AboutPage} />
-      {/* <Route path="/blog" component={BlogPage} /> */}
+      <Route path="/blog" component={BlogPage} />
       <Route path="/contact" component={ContactPage} />
       <Route component={NotFound} />
     </Switch>
